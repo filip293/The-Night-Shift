0793c4fbe49af600dcb2682fadc6a1add9d41656
Filip <filipgamer987@gmail.com>
2026-07-19 15:52:47 +0200

    Fixes

--- Player.gd
+++ Player.gd
@@ -25,7 +25,7 @@ func _physics_process(delta: float) -> void:
 
 			
 
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backwards")
+	var input_dir := Input.get_vector("Left", "Right", "Forward", "Back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	if Globals.playermoveallow and direction.length() > 0.1:
