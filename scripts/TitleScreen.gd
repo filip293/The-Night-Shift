60a647fd21789d80f9773d1f54602cddf1cf9ce4
squaapi <squaapi@gmail.com>
2026-07-22 03:10:57 +0200

    TitleScreen update

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -1,24 +1,25 @@
 extends Node2D
 
-@onready var TitleCam := $"../TitleCamera"
+@onready var TitleCam := $"../Camera/TitleCamera"
 @onready var Player := $"../Player"
 @onready var Map := $"../Map"
 @onready var PlayerCam := $"../Player/Neck/Camera"
 
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CONFINED
 	Player.process_mode = Node.PROCESS_MODE_DISABLED
 	Map.process_mode = Node.PROCESS_MODE_DISABLED
 	TitleCam.make_current()
-	#PLAY ANIMATION HERE
 	$CanvasLayer.visible = true
+	$CanvasLayer/Animations.play("startup")
+	await $CanvasLayer/Animations.animation_finished
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_start_pressed() -> void:
 	$CanvasLayer.visible = false
-	#ADD TRANSITION HERE
+	#TRANSITION TO GAME
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	PlayerCam.make_current()
 	Player.process_mode = Node.PROCESS_MODE_INHERIT
