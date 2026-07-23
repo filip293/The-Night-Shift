ca6052c2db464fecb471b2aafbaf72f3693bea98
squaapi <squaapi@gmail.com>
2026-07-23 15:48:09 +0200

    Fixed a couple things and imported more assets

--- scripts/TitleScreen.gd
+++ scripts/TitleScreen.gd
@@ -1,16 +1,20 @@
 extends Node2D
 
 @onready var TitleCam := $"../Camera/TitleCamera"
+@onready var TitleCamAnim := $"../Camera/AnimationPlayer"
 @onready var Player := $"../Player"
 @onready var Map := $"../Map"
 @onready var PlayerCam := $"../Player/Neck/Camera"
+@onready var BGM := $BackgroundMusic
 
 func _ready() -> void:
 	Player.process_mode = Node.PROCESS_MODE_DISABLED
 	Map.process_mode = Node.PROCESS_MODE_DISABLED
 	TitleCam.make_current()
 	$CanvasLayer.visible = true
+	TitleCamAnim.play("CamAnim/handheld_sway")
 	$CanvasLayer/Animations.play("startup")
+	BGM.autoplay = true
 	await $CanvasLayer/Animations.animation_finished
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
@@ -18,9 +22,17 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_start_pressed() -> void:
+	$CanvasLayer/Animations.play("fadetitle")
+	await $CanvasLayer/Animations.animation_finished
 	$CanvasLayer.visible = false
-	#TRANSITION TO GAME
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$CanvasLayer/Animations.play("fade")
+	await $CanvasLayer/Animations.animation_finished
+	BGM.stop() # I might make it very quiet while the boss is calling...
+	BGM.autoplay = false
 	PlayerCam.make_current()
+	$CanvasLayer/Animations.play_backwards("fade")
+	await $CanvasLayer/Animations.animation_finished
+	$BlackScreen.visible = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	Player.process_mode = Node.PROCESS_MODE_INHERIT
 	Map.process_mode = Node.PROCESS_MODE_INHERIT
