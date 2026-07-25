c487935db51e84ff75e48776e2f39e13435d58e2
squaapi <squaapi@gmail.com>
2026-07-25 15:33:08 +0200

    Base template for TaskManager

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -7,6 +7,7 @@ var mouse_sensitivity = 0.1
 var playermoveallow = true
 var playerlookallow = true
 var stationcar = false
+var task_idx = 0
 var player_keys: Array[String] = []
 
 func calltime(time) -> void:
