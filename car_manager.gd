2b8a4246c6542fcd87fc697739966f03ae5f5622
squaapi <squaapi@gmail.com>
2026-07-25 17:06:35 +0200

    Cars

--- car_manager.gd
+++ car_manager.gd
@@ -1,4 +1,19 @@
 extends Node
 
+@onready var FirstCar: AnimationPlayer = $"../FirstCar/Animations"
+@onready var SecondCar: AnimationPlayer = $"../SecondCar/Animations"
+
+signal car1_done
+
 func _ready() -> void:
-	Globals.calltime
+	await Globals.calltime(20.0)
+	Globals.stationcar = true
+	await Globals.calltime(10.0)
+	go_car1()
+	await Globals.calltime(5.0)
+	Globals.stationcar = false
+	await car1_done
+	
+func go_car1() -> void:
+	FirstCar.play("ENTER")
+	
