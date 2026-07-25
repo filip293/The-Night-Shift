8dd3b01452cd0a3aec9ff886ce37b4f955bf045b
squaapi <squaapi@gmail.com>
2026-07-25 17:08:21 +0200

    Merge

--- Interactable.gd
+++ Interactable.gd
@@ -1,28 +1,44 @@
+@tool
 extends StaticBody3D
 
 @export var whoami_value: String = "Object"
 
-enum ObjectType { GENERIC, DOOR }
-@export var object_type: ObjectType = ObjectType.GENERIC
+enum ObjectType { GENERIC, DOOR, PUDDLE }
+@export var object_type: ObjectType = ObjectType.GENERIC:
+	set(value):
+		object_type = value
+		notify_property_list_changed()
 
+# --- Door Settings ---
+@export_group("Door Settings")
 @export var open_rotation_y: float = 90.0
 @export var closed_rotation_y: float = 0.0
 @export var audio_player: AudioStreamPlayer3D
 @export var open_sound: AudioStream
 @export var close_sound: AudioStream
 
+# --- Puddle Settings ---
+@export_group("Puddle Settings")
+@export var puddle_mesh: MeshInstance3D
+@export var mop_animation_player: AnimationPlayer
+@export var mopping_sound: AudioStream
+@export var mop_duration: float = 1.5
+
 var is_open: bool = false
 var door_tween: Tween
+var is_being_mopped: bool = false
+var current_mop_progress: float = 0.0  # Keeps track of progress even if paused
 
-func _get_property_list():
-	var properties = []
-	if object_type == ObjectType.DOOR:
-		properties.append({ "name": "open_rotation_y", "type": TYPE_FLOAT })
-		properties.append({ "name": "closed_rotation_y", "type": TYPE_FLOAT })
-		properties.append({ "name": "audio_player", "type": TYPE_NODE_PATH, "hint_string": "AudioStreamPlayer3D" })
-		properties.append({ "name": "open_sound", "type": TYPE_OBJECT, "hint_string": "AudioStream" })
-		properties.append({ "name": "close_sound", "type": TYPE_OBJECT, "hint_string": "AudioStream" })
-	return properties
+func _validate_property(property: Dictionary) -> void:
+	# Handle Door settings visibility
+	var door_properties = ["open_rotation_y", "closed_rotation_y", "audio_player", "open_sound", "close_sound"]
+	if property.name in door_properties and object_type != ObjectType.DOOR:
+		property.usage = PROPERTY_USAGE_NO_EDITOR
+
+	# Handle Puddle settings visibility
+	var puddle_properties = ["puddle_mesh", "mop_animation_player", "mopping_sound", "mop_duration"]
+	if property.name in puddle_properties and object_type != ObjectType.PUDDLE:
+		property.usage = PROPERTY_USAGE_NO_EDITOR
 
 func interact() -> void:
 	match object_type:
@@ -30,34 +46,121 @@ func interact() -> void:
 			_toggle_door()
 		ObjectType.GENERIC:
 			_on_interact_generic()
+		# Puddle is now handled continuously by the Raycast via start_mopping()
 
 func _toggle_door() -> void:
-	if door_tween and door_tween.is_running():
+	if Engine.is_editor_hint() or (door_tween and door_tween.is_running()):
 		return
 
 	is_open = not is_open
-	
 	door_tween = create_tween()
 
+	var target_y = open_rotation_y if is_open else closed_rotation_y
+	var current_y = rotation_degrees.y
+	var angle_difference = wrapf(target_y - current_y, -180.0, 180.0)
+	var final_target_y = current_y + angle_difference
+
 	if is_open:
-		var open_duration = 1.5
 		if audio_player and open_sound:
 			audio_player.stream = open_sound
 			audio_player.play()
-			
-		door_tween.tween_property(self, "rotation_degrees:y", open_rotation_y, open_duration)\
-			.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+		door_tween.tween_property(self, "rotation_degrees:y", final_target_y, 1.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
 	else: 
-		var close_duration = 1.5
 		if audio_player and close_sound:
 			audio_player.stream = close_sound
 			audio_player.play()
-			
-		door_tween.tween_property(self, "rotation_degrees:y", closed_rotation_y, close_duration)\
-			.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+		door_tween.tween_property(self, "rotation_degrees:y", final_target_y, 1.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+
+# Called once when the player first presses and holds E on the puddle
+func start_mopping() -> void:
+	if Engine.is_editor_hint() or is_being_mopped:
+		return
+		
+	is_being_mopped = true
+	
+	# Lock player position and look angles
+	if "playermoveallow" in Globals:
+		Globals.playermoveallow = false
+	if "playerlookallow" in Globals:
+		Globals.playerlookallow = false
+
+	# Play Mopping Sound
+	if audio_player and mopping_sound:
+		audio_player.stream = mopping_sound
+		audio_player.play()
+
+	# Play & loop the mopping animation programmatically
+	if mop_animation_player and mop_animation_player.has_animation("Bwoom"):
+		var anim = mop_animation_player.get_animation("Bwoom")
+		if anim:
+			anim.loop_mode = Animation.LOOP_LINEAR
+		mop_animation_player.play("Bwoom") 
+
+	# Duplicate material to prevent fading other puddles simultaneously
+	if puddle_mesh and puddle_mesh.get_active_material(0) and not puddle_mesh.material_override:
+		var mat = puddle_mesh.get_active_material(0).duplicate()
+		puddle_mesh.material_override = mat
+
+# Called every frame while the player is successfully holding down E
+func mop_tick(delta: float) -> void:
+	if not is_being_mopped:
+		return
+		
+	current_mop_progress += delta
+	
+	# Update opacity based on progress
+	if puddle_mesh and puddle_mesh.material_override:
+		var progress_pct = current_mop_progress / mop_duration
+		puddle_mesh.material_override.albedo_color.a = clampf(1.0 - progress_pct, 0.0, 1.0)
+		
+	# Check for completion
+	if current_mop_progress >= mop_duration:
+		_finish_mopping()
+
+# Called automatically when they release E before the puddle is fully cleaned
+func cancel_mopping() -> void:
+	if not is_being_mopped:
+		return
+		
+	is_being_mopped = false
+	
+	# Unlock player
+	if "playermoveallow" in Globals:
+		Globals.playermoveallow = true
+	if "playerlookallow" in Globals:
+		Globals.playerlookallow = true
+
+	# Stop sounds and animations
+	if audio_player:
+		audio_player.stop()
+	if mop_animation_player:
+		mop_animation_player.stop()
+
+func _finish_mopping() -> void:
+	is_being_mopped = false
+	
+	# Disable collision immediately
+	var col_shape = get_node_or_null("CollisionShape3D")
+	if col_shape:
+		col_shape.set_deferred("disabled", true)
+
+	# Unlock player
+	if "playermoveallow" in Globals:
+		Globals.playermoveallow = true
+	if "playerlookallow" in Globals:
+		Globals.playerlookallow = true
+
+	# Stop sound & animations
+	if audio_player:
+		audio_player.stop()
+	if mop_animation_player:
+		mop_animation_player.stop()
+
+	queue_free()
 
 func _on_interact_generic() -> void:
-	print("Interacted with generic object: ", whoami_value)
+	if not Engine.is_editor_hint():
+		print("Interacted with generic object: ", whoami_value)
 
 func whoami() -> String:
 	return whoami_value
--- RCastLogic.gd
+++ RCastLogic.gd
@@ -3,13 +3,15 @@ extends RayCast3D
 @export_group("UI Elements")
 @export var label: Label
 
-enum ObjectType { GENERIC, DOOR }
+enum ObjectType { GENERIC, DOOR, PUDDLE }
 
 var label_tween: Tween
 var current_displayed_text: String = ""
 
-func _ready() -> void:
+# Track the puddle currently being mopped
+var active_puddle: Node3D = null
 
+func _ready() -> void:
 	enabled = true
 	if label:
 		label.text = ""
@@ -18,6 +20,24 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var target_text = ""
 
+	# If we are currently in the middle of mopping a puddle
+	if is_instance_valid(active_puddle):
+		if Input.is_action_pressed("Interact"):
+			target_text = "Mopping..."
+			active_puddle.mop_tick(delta)
+			
+			# If the puddle finished and was deleted during this tick, clear the state
+			if not is_instance_valid(active_puddle):
+				target_text = ""
+		else:
+			# Player let go of the E key before finishing
+			active_puddle.cancel_mopping()
+			active_puddle = null
+			
+		_animate_label(target_text)
+		return
+
+	# Normal Raycast collision checking
 	if is_colliding():
 		var collider = get_collider()
 		if collider and collider.has_method("interact") and "object_type" in collider:
@@ -31,9 +51,20 @@ func _physics_process(delta: float) -> void:
 			elif object_type == ObjectType.GENERIC:
 				var object_name = collider.whoami() if collider.has_method("whoami") else "Object"
 				target_text = "[E] Use " + object_name
-				if Input.is_action_just_pressed("Interact") and object_name == "Broom":
+				if Input.is_action_just_pressed("Interact") and object_name == "Mop":
 					$"../../../../BucketAndMop/StaticBody3D".visible = false
-					$"../../../Broom_Cylinder_005".visible = true
+					$"../../../Bwooom".visible = true
+					
+			elif object_type == ObjectType.PUDDLE:
+				var has_mop = $"../../../Bwooom".visible
+				
+				if has_mop:
+					target_text = "[E] Mop Puddle"
+					if Input.is_action_just_pressed("Interact"):
+						active_puddle = collider
+						collider.start_mopping()
+				else:
+					target_text = "I need a mop first."
 
 	_animate_label(target_text)
 
