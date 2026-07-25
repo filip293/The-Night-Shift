2948c0e28e63887bd7dbc2db9ef68796924a1328
Filip <filipgamer987@gmail.com>
2026-07-25 16:06:45 +0200

    Fix

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -1,8 +1,5 @@
 extends Node
 
-@onready var BaseTime = $/root/Node3D/BaseTime
-signal timeend
-
 var mouse_sensitivity = 0.1
 var playermoveallow = true
 var playerlookallow = true
@@ -10,8 +7,15 @@ var stationcar = false
 var task_idx = 0
 var player_keys: Array[String] = []
 
-func calltime(time) -> void:
-	BaseTime.set_wait_time(time)
-	BaseTime.start()
-	await BaseTime.timeout
-	timeend.emit()
+func calltime(time: float) -> Signal:
+	var timer := Timer.new()
+	timer.process_mode = Node.PROCESS_MODE_ALWAYS
+	timer.wait_time = time
+	timer.one_shot = true
+	
+	timer.timeout.connect(timer.queue_free)
+	
+	add_child(timer)
+	timer.start()
+	
+	return timer.timeout
