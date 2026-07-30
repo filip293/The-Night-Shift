a5113233b1d1dee7d289936a685c5a450508c637
squaapi <squaapi@gmail.com>
2026-07-30 13:48:38 +0200

    here ya go

--- receipt.gd
+++ receipt.gd
@@ -3,13 +3,26 @@ extends CanvasLayer
 @onready var modulator := $CanvasModulate
 @export var fade_duration: float
 
+@export var sprite: Sprite2D
+@export var task1: Texture2D
+@export var task2: Texture2D
+@export var task3: Texture2D
 @export var hidden_color: Color
 @export var visible_color: Color = Color.WHITE
 
 var fade: Tween
 
+func _ready() -> void:
+	modulator.color = hidden_color
+	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ReceiptOpen"):
+		if Globals.task_idx == 1: 
+			sprite.texture = task1
+		elif Globals.task_idx == 2: 
+			sprite.texture = task2
+		elif Globals.task_idx == 3:
+			sprite.texture = task3
 		animate_to(visible_color)
 	elif event.is_action_released("ReceiptOpen"):
 		animate_to(hidden_color)
